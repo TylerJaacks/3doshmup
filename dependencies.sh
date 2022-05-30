0b689705b652abefe1c2a7e49236ff6b0ed003b6
Tyler Jaacks <TylerJaacks@Gmail.com>
2022-05-30 00:08:05 -0500

    Added a dependency script, for future GitHub actions.

---
+++ dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Get the 3do-devkit
+git clone https://github.com/trapexit/3do-devkit.git && cd 3do-devkit
+
+# Set the environment variable for the 3do-devkit.
+export THREEDO_DEVKIT=`pwd`
